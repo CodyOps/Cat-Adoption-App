8c1fdfcf9582f5a7e15b07312c43298819f1d3cd
Christian Go <cvgo2000@yahoo.com>
2021-01-15 20:35:03 -0700

    Updated match.js file

---
+++ models/match.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+const Sequelize = require("sequelize");
+// Sequelize (lowercase) references our connection to the DB.
+const sequelize = require("../config/catmarketconnection.js");
+
+// Creates a "name" model that matches up with breed in DB
+const Match = sequelize.define(
+  "match",
+  {
+    // The routeName gets saved as a string
+    routeName: Sequelize.STRING,
+    // The type of cat breed (a string)
+    name: Sequelize.STRING,
+    // First cat trait (a string)
+    temperamentOne: Sequelize.STRING,
+    // Second cat trait (a string)
+    temperatmentTwo: Sequelize.INTEGER,
+    // Third cat trait (a string)
+    temperatmentThree: Sequelize.INTEGER,
+    // Breed's maximum life expectancy
+    lifeSpan: Sequelize.INTEGER,
+    // Breed's general size rating
+    size: Sequelize.STRING,
+    // Wikipedia link for more information
+    wikipediaLink: Sequelize.STRING,
+  },
+  {
+    // Disable the modification of tablenames; By default, sequelize will automatically
+    // Transform all passed model names (first parameter of define) into plural. If not set:
+    freezeTableName: true,
+  }
+);
+
+// Syncs with DB
+Match.sync();
+
+// Makes the Character Model available for other files (will also create a table)
+module.exports = Match;
